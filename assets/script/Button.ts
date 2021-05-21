81ad932256b31119795ab7552c81ec8e5e7cf35f
duardoqueiroz <eduardoldq1@gmail.com>
2021-05-21 19:07:06 -0300

    fix: main menu scene

--- assets/script/Button.ts
+++ assets/script/Button.ts
@@ -1,13 +1,12 @@
-
-const {ccclass} = cc._decorator;
+const { ccclass } = cc._decorator;
 
 @ccclass
 export default class Button extends cc.Component {
 
     public nextStep(): void {
-        cc.director.loadScene("char_selection")
+        cc.director.loadScene("local_selection");
     }
     public backStep(): void {
-        cc.director.loadScene("Menu")
+        cc.director.loadScene("Menu");
     }
 }
