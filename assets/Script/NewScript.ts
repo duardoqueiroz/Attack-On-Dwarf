5154da10b944ce4518f752c22134b1d9382a8895
Letícia Duarte <leticiabsduarte@gmail.com>
2021-05-21 17:25:37 -0300

    chore: new scene added
    a scene to choose the map with buttons and their respectives icons and a button to get back to the main scene

--- assets/Script/NewScript.ts
+++ assets/Script/NewScript.ts
@@ -1,16 +1,13 @@
 
-const {ccclass, property} = cc._decorator;
+const {ccclass} = cc._decorator;
 
 @ccclass
 export default class Button extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
     public nextStep(): void {
         cc.director.loadScene("char_selection")
+    }
+    public backStep(): void {
+        cc.director.loadScene("Menu")
     }
 }
