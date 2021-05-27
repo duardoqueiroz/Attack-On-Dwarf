7c03f3e1c146b7d9843cb2f753646d6461b73468
Letícia Duarte <leticiabsduarte@gmail.com>
2021-05-27 17:46:54 -0300

    fix: keyboard controller error solved

--- assets/scripts/controller/KeyboardController.ts
+++ assets/scripts/controller/KeyboardController.ts
@@ -12,4 +12,3 @@ export default abstract class KeyboardController extends cc.Component {
     protected abstract onKeyUp(event: cc.Event.EventKeyboard);
 
 }
-s
--- assets/scripts/controller/MMKeyboardController.ts
+++ assets/scripts/controller/MMKeyboardController.ts
@@ -17,4 +17,3 @@ export default class MainmenuKeyboardController extends KeyboardController {
 
 
 }
-s
