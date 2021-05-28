9a18f2bf0ddc263d94143a8e9a8939352381c0ad
duardoqueiroz <eduardoldq1@gmail.com>
2021-05-27 21:31:43 -0300

    feat: added physics and collision settings

---
+++ assets/scripts/Main.ts
@@ -0,0 +1,15 @@
+const { ccclass } = cc._decorator;
+
+@ccclass
+export default class Main extends cc.Component {
+    private physicsManager: cc.PhysicsManager;
+    private collisionManager: cc.CollisionManager;
+
+    private onLoad() {
+      this.physicsManager = cc.director.getPhysicsManager();
+      this.physicsManager.enabled = true;
+      this.physicsManager.gravity = cc.v2(0, -2000);
+      this.collisionManager = cc.director.getCollisionManager();
+      this.collisionManager.enabled = true;
+    }
+}
