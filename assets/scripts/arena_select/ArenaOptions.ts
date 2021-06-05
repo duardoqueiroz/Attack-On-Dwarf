f166dceab377931ae6d9b1b78e3abca9303adf46
duardoqueiroz <eduardoldq1@gmail.com>
2021-06-05 17:12:29 -0300

    fix:  ArenaOptions extends SelectOptions

--- assets/scripts/arena_select/ArenaOptions.ts
+++ assets/scripts/arena_select/ArenaOptions.ts
@@ -1,12 +1,9 @@
+import SelectOptions from "../SelectOptions";
 import iSpriteWithName from "./iSpriteWithName";
-const {ccclass} = cc._decorator;
 
+const { ccclass } = cc._decorator;
 @ccclass
-export default class ArenaOptions extends cc.Component {
-
-    private spriteList: iSpriteWithName[] = [];
-
-    private _selectedIndex: number = 0;
+export default class ArenaOptions extends SelectOptions {
 
     private get selectedIndex() {
         return this._selectedIndex;
@@ -18,45 +15,20 @@ export default class ArenaOptions extends cc.Component {
         this._selectedIndex = newIndex;
     }
 
-    public onLoad () {
-        this.spriteList = this.node.children.map((c) => {
-            return{
-                sprite: c.getComponent(cc.Sprite),
-            }; 
-        });
-        this.selectedIndex = 0;
-    }
-
-    public selectUp() {
-        if (this.selectedIndex > 0) {
-            this.selectedIndex--;
-        }else {
-            this.selectedIndex =  this.spriteList.length - 1;
-        }
-    }
-
-    public selectDown() {
-        if (this.selectedIndex < this.spriteList.length - 1){
-           this.selectedIndex++; 
-        } else{
-           this.selectedIndex = 0; 
-        } 
-    }
-
     public selectOption() {
-        if (this.selectedIndex== 0) {
+        if (this.selectedIndex == 0) {
             cc.director.loadScene('shopping');
         }
-        if (this.selectedIndex== 1) {
+        if (this.selectedIndex == 1) {
             cc.director.loadScene('center');
         }
-        if (this.selectedIndex== 2) {
+        if (this.selectedIndex == 2) {
             cc.director.loadScene('place');
         }
-        if (this.selectedIndex== 3) {
+        if (this.selectedIndex == 3) {
             cc.director.loadScene('cinesystem');
         }
-        if (this.selectedIndex== 4) {
+        if (this.selectedIndex == 4) {
             cc.director.loadScene('trevo');
         }
     }
