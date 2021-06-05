4e32323634949ebed5ff432c187fccc15cf57cd4
duardoqueiroz <eduardoldq1@gmail.com>
2021-06-05 17:06:31 -0300

    feat: character selection keyboard controller

---
+++ assets/scripts/controller/SelectCharacterKeyboardController.ts
@@ -0,0 +1,27 @@
+import KeyboardController from "./KeyboardController";
+import CharacterOptions from "../character_select/CharacterOptions";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class SelectCharacterKeyboardController extends KeyboardController {
+    
+    @property(CharacterOptions)
+    private character:CharacterOptions = null;
+    
+    protected onKeyDown(event: cc.Event.EventKeyboard) {
+        switch(event.keyCode){
+            case cc.macro.KEY.w:
+                this.character.selectUp();
+                break;
+            case cc.macro.KEY.s:
+                this.character.selectDown();
+                break;
+            case cc.macro.KEY.enter:
+                this.character.selectOption();
+                break;
+        }
+    }
+
+
+}
