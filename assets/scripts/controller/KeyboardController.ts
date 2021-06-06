9be1ba398fa44fcf8059dd72ef25ce561dc3bc1d
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-06-06 20:28:23 -0300

    fix: renamed files

--- assets/scripts/controller/KeyboardController.ts
+++ assets/scripts/controller/KeyboardController.ts
@@ -2,13 +2,12 @@ const { ccclass } = cc._decorator;
 
 @ccclass
 export default abstract class KeyboardController extends cc.Component {
-    public onLoad() {
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-    }
+  public onLoad(): void {
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+  }
 
-    protected abstract onKeyDown(event: cc.Event.EventKeyboard);
+  protected abstract onKeyDown(event: cc.Event.EventKeyboard): void;
 
-    protected abstract onKeyUp(event: cc.Event.EventKeyboard);
-
+  protected abstract onKeyUp(event: cc.Event.EventKeyboard): void;
 }
--- assets/scripts/controller/MainmenuKeyboardController.ts
+++ assets/scripts/controller/MainmenuKeyboardController.ts
@@ -1,15 +1,18 @@
-import KeyboardController from "./KeyboardController";
+import KeyboardController from './KeyboardController';
 
-const {ccclass} = cc._decorator;
+const { ccclass } = cc._decorator;
 
 @ccclass
 export default class MainmenuKeyboardController extends KeyboardController {
-    protected onKeyDown(event: cc.Event.EventKeyboard) {
-        switch (event.keyCode) {
-            case cc.macro.KEY.enter:
-                cc.director.loadScene("load_map");
-                break;
-        }
-    }
+  protected onKeyUp(_event: cc.Event.EventKeyboard): void {
+    throw new Error('Method not implemented.');
+  }
 
+  protected onKeyDown(event: cc.Event.EventKeyboard): void {
+    switch (event.keyCode) {
+      case cc.macro.KEY.enter:
+        cc.director.loadScene('load_map');
+        break;
+    }
+  }
 }
