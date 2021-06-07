69816c5372030c977594278a64045e524512698b
duardoqueiroz <eduardoldq1@gmail.com>
2021-06-07 15:13:12 -0300

    fix: folders renamed

--- assets/scripts/controller/MainmenuKeyboardController.ts
+++ assets/scripts/controller/MainmenuKeyboardController.ts
@@ -1,10 +1,10 @@
 import KeyboardController from "./KeyboardController";
 
-const {ccclass} = cc._decorator;
+const { ccclass } = cc._decorator;
 
 @ccclass
-export default class MainmenuKeyboardController extends KeyboardController {
-    protected onKeyDown(event: cc.Event.EventKeyboard) {
+export default class MainMenuKeyboardController extends KeyboardController {
+    protected onKeyDown(event: cc.Event.EventKeyboard): void {
         switch (event.keyCode) {
             case cc.macro.KEY.enter:
                 cc.director.loadScene("load_map");
