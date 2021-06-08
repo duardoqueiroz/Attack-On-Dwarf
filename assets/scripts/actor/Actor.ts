bd45296505de2c4f92039b78668d6b2a7975dd55
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-06-08 14:56:04 -0300

    chore: add movement script to Goku prefab

--- assets/scripts/actor/Actor.ts
+++ assets/scripts/actor/Actor.ts
@@ -1,97 +1,97 @@
-import DIRECTION, { DIRECTIONS } from "./DIRECTION";
-import { IWalking } from "./IWalking";
+import DIRECTION, { DIRECTIONS } from './DIRECTION';
+import { IWalking } from './IWalking';
 import PLAYER_STATE from './PLAYER_STATE';
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Actor extends cc.Component {
+  private _state: PLAYER_STATE = PLAYER_STATE.idle;
+  public facing: DIRECTION = DIRECTIONS.right;
+  public moving: boolean = false;
 
-    private _state: PLAYER_STATE = PLAYER_STATE.idle;
-    public facing: DIRECTION = DIRECTIONS.right;
-    public moving:boolean = false;
-    
-    private _isJumping: boolean = false;
+  private _isJumping: boolean = false;
 
-    public get isJumping(): boolean {
-        return this._isJumping;
+  public get isJumping(): boolean {
+    return this._isJumping;
+  }
+  public set isJumping(value: boolean) {
+    if (!value && this.state === PLAYER_STATE.jumping) {
+      this.state = PLAYER_STATE.idle;
+    } else {
+      this._isJumping = value;
     }
-    public set isJumping(value: boolean) {
-        if (!value && this.state === PLAYER_STATE.jumping) {
-            this.state = PLAYER_STATE.idle;
-        }else{
-        this._isJumping = value;
-        }
+  }
+
+  @property(cc.Float)
+  public jumpForce: number = 600000;
+
+  @property(cc.Float)
+  public walkForce: number = 80000;
+
+  @property(cc.Float)
+  public maxSpeed: number = 400;
+
+  public rigidBody: cc.RigidBody;
+
+  public get isInTheFloor() {
+    return this.rigidBody.linearVelocity.y === 0 && !this.isJumping;
+  }
+
+  public onLoad() {
+    this.rigidBody = this.getComponent(cc.RigidBody);
+  }
+
+  onBeginContact(contact: any, selfCollider: cc.Collider, otherCollider: cc.Collider) {
+    if (selfCollider.tag == 2 && this.isJumping) {
+      this.isJumping = false;
     }
+  }
 
-    @property(cc.Float)
-    public jumpForce: number = 600000;
+  public get state() {
+    return this._state;
+  }
 
-    @property(cc.Float)
-    public walkForce: number = 80000;
-
-    @property(cc.Float)
-    public maxSpeed: number = 400;
-
-    public rigidBody: cc.RigidBody;
-
-    public get isInTheFloor() {
-        return this.rigidBody.linearVelocity.y === 0 && !this.isJumping;
+  public set state(newState: PLAYER_STATE) {
+    if (this._state != newState) {
+      this.getComponent(cc.Animation).play(newState);
+      this._state = newState;
     }
+  }
 
-    public onLoad () {
-        this.rigidBody = this.getComponent(cc.RigidBody);
+  public jump(): void {
+    if (this.isInTheFloor) {
+      console.log('oi');
+      this.state = PLAYER_STATE.jumping;
+      this.isJumping = true;
+      this.rigidBody.applyForceToCenter(cc.v2(0, this.jumpForce), true);
+      console.log('ta pulando poha?');
     }
+  }
 
-    onBeginContact(contact: any, selfCollider: cc.Collider, otherCollider: cc.Collider) {
-        if (selfCollider.tag == 2 && this.isJumping) {
-            this.isJumping = false;
-        }
-    }
-
-    public get state() {
-        return this._state;
-    }
-
-    public set state(newState: PLAYER_STATE) {
-        if (this._state != newState) {
-            this.getComponent(cc.Animation).play(newState);
-            this._state = newState;
-        }
-    }
-
-    public jump():void {
-       if (this.isInTheFloor) {
-            console.log('oi');
-            this.state = PLAYER_STATE.jumping;
-            this.isJumping = true;
-            this.rigidBody.applyForceToCenter(cc.v2(0, this.jumpForce), true);
-            console.log("ta pulando poha?");
-        }
-    }
-
-  move(direction: DIRECTION){
+  move(direction: DIRECTION) {
     this.moving = true;
     this.movePlayer(direction);
     if (direction != this.facing) {
-        this.turnAround(direction);
+      this.turnAround(direction);
     } else {
-        if (!this.isJumping) {
-            this.state = PLAYER_STATE.running;
-        }
+      if (!this.isJumping) {
+        this.state = PLAYER_STATE.running;
+      }
     }
+  }
+
+  private movePlayer(direction: DIRECTION): void {
+    if (
+      Math.abs(this.rigidBody.linearVelocity.x) < this.maxSpeed &&
+      Math.abs(this.rigidBody.linearVelocity.y) < this.maxSpeed
+    ) {
+      this.rigidBody.applyForceToCenter(cc.v2(direction.x * this.walkForce, direction.y * this.walkForce), true);
     }
+  }
 
-    private movePlayer(direction: DIRECTION):void {
-        if (Math.abs(this.rigidBody.linearVelocity.x) < this.maxSpeed && Math.abs(this.rigidBody.linearVelocity.y) < this.maxSpeed) {
-            this.rigidBody.applyForceToCenter(cc.v2(direction.x * this.walkForce, direction.y * this.walkForce), true);
-        }
-    }
-
-    private turnAround(direction: DIRECTION):void {
-        this.node.scaleX = this.node.scaleX * -1;
-        this.facing = direction;
-    }
-
-
+  private turnAround(direction: DIRECTION): void {
+    this.node.scaleX = this.node.scaleX * -1;
+    this.facing = direction;
+  }
 }
--- assets/scripts/player_select/SetPlayerOnScene.ts
+++ assets/scripts/player_select/SetPlayerOnScene.ts
@@ -5,14 +5,11 @@ export default class SetPlayerOnScene extends cc.Component {
 
   @property(cc.Prefab)
   goku: cc.Prefab = null;
-  
-  cont: number = 0;
 
-  start(){
-      const gocu = cc.instantiate(this.goku);
-      gocu.x = 174;
-      gocu.y = 252;
-      this.node.addChild(gocu);
-    
+  start() {
+    const gocu = cc.instantiate(this.goku);
+    gocu.x = 174;
+    gocu.y = 252;
+    this.node.addChild(gocu);
   }
 }
